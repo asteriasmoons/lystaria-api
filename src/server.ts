1c4f0452c06318a95af15ff6a9da23cd8e3401d7
Asteria Moon <asteriasmoons@outlook.com>
2026-03-07 11:27:00 -0600

    Mounted recs endpoint

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import summaryRoute from "./routes/summary";
+import recsRoute from "./routes/recs";
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.get("/", (req, res) => {
 
 // Routes
 app.use("/api/books/summary", summaryRoute);
+app.use("/api/books/recs", recsRoute);
 
 // Start server
 app.listen(PORT, () => {
